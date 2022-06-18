54630ef0e6f777adf4e79523dd008f620775ff8e
harry <harry@citi.sinica.edu.tw>
2022-06-18 22:05:48 +0800

    Add api for list commit. hash, value's type change

--- libraries/core/commit/commit.go
+++ libraries/core/commit/commit.go
@@ -1,52 +1,54 @@
 package main
 
-import "time"
+import (
+	"time"
+	"sort"
+	//"fmt"
+	//"Digit/libraries/core/diff"
+)
 //struct
 
-type Hash struct{
-	hash_value [20]byte
-	// Need to discuss hash struct details
-}
-
 type Commit struct {
-	//prev *Node
-	//next *Node
 	time time.Time
 	author string
-	hash Hash
-	value string// type Value when sync
+	hash string
+	value ProllyTree // type Value when sync
+	message string
 }
 
 type Head struct{
 	// head record head commit's hash
-	head Hash
+	CommitHash string //hash encode->string
 	// branch implementation
 	// parent *[]
 }
 
 //global variable
 
-var commit_graph map[Hash]Commit
+var commit_graph map[string]Commit
 var Head_pt Head
 
 // functions
 
 //init map
 func init(){
-	commit_graph=make(map[Hash]Commit)
+	commit_graph=make(map[string]Commit)
 }
 
 // set head
-func set_head(hash Hash){
-	Head_pt.head=hash
+func set_head(hash string){
+	Head_pt.CommitHash=hash
 }
+
 // new commit
-func new_commit(time time.Time, author string, hash Hash, value string){
+func new_commit(time time.Time, author string, hash string, value ProllyTree, message string){
+	copyprollytree:=value
 	node:=Commit{
 		time: time,
 		author: author,
 		hash: hash,
-		value: value,
+		value: copyprollytree,
+		message: message,
 	}
 	//fmt.Printf("finish")
 	commit_graph[hash]=node
@@ -55,13 +57,25 @@ func new_commit(time time.Time, author string, hash Hash, value string){
 
 // find head
 func find_head () Commit{
-	return commit_graph[Head_pt.head]
+	return commit_graph[Head_pt.CommitHash]
 }
+
 // find commit
-func find_commit (hash Hash) Commit{
+func find_commit (hash string) Commit{
 	return commit_graph[hash]
 }
+
 //reset
-func reset(hash Hash){
-	Head_pt.head=hash
+func reset(hash string){
+	Head_pt.CommitHash=hash
+}
+
+// log , sort by time
+func listcommit() []Commit{
+	list:=make([]Commit,len(commit_graph))
+	for _,val :=range commit_graph{
+		list=append(list,val)
+	}
+	sort.Slice(list, func(i, j int) bool {return list[i].time.After(list[j].time)})
+	return list
 }
