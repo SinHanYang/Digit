57564afb531dc58293643e3ad863d0ad1d42fbe1
itzsyboo <55495526+Fiona1121@users.noreply.github.com>
2022-06-18 14:21:39 +0800

    modify commit cmd

--- cmd/digit/commit.go
+++ cmd/digit/commit.go
@@ -1,12 +1,22 @@
 package digit
 
 import (
+	"log"
+
 	"github.com/spf13/cobra"
 )
 
-func Commit(m string, rest []string) {
+func Commit(a string, m string, rest []string) {
 	// TODO: Commit implement
-
+	if a == "" {
+		log.Fatal("please tell me who you are")
+	}
+	if m == "" {
+		log.Fatal("commit message cannot be empty")
+	}
+	// var hash Hash
+	// hash.hash_value = "hash"
+	// t := time.Now()
 }
 
 var commitCmd = &cobra.Command{
@@ -14,12 +24,16 @@ var commitCmd = &cobra.Command{
 	Short: "commit",
 	Long:  "commit",
 	Run: func(cmd *cobra.Command, args []string) {
+		a, _ := cmd.Flags().GetString("author")
 		m, _ := cmd.Flags().GetString("message")
-		Commit(m, args)
+		Commit(a, m, args)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(commitCmd)
+	commitCmd.Flags().StringP("author", "a", "", "commit author")
 	commitCmd.Flags().StringP("message", "m", "", "commit message")
+	commitCmd.MarkFlagRequired("author")
+	commitCmd.MarkFlagRequired("message")
 }
